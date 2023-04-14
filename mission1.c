8250fa73ddeab53a324ef2a6f1a1155a6e09a10c
보민 <129143463+sightoftiger@users.noreply.github.com>
2023-04-14 02:21:20 +0000

    add else_if_2

--- mission1.c
+++ mission1.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
-main (){
-  int a;
+// #include <stdio.h>
+// main (){
+//   int a;
   
-  printf("정수를 입력하세요 : ");
-  scanf ("%d", &a); //&는 주소
+//   printf("정수를 입력하세요 : ");
+//   scanf ("%d", &a); //&는 주소
 
-  if (a%2==0){
-    printf("짝수입니다");
-  }
-  else{
-    printf("홀수입니다");
-  }
-}
+//   if (a%2==0){
+//     printf("짝수입니다");
+//   }
+//   else{
+//     printf("홀수입니다");
+//   }
+// }
